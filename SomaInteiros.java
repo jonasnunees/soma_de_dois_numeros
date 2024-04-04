e8800e7af8d698382682b4b66bdafc277ba43bd4
jonasnunees <jonas.nunees@hotmail.com>
2024-04-04 20:43:58 -0300

    Create SomaInteiros.java
    Declarando duas variáveis do tipo inteiro com números distintos, depois colocando o resultado da soma entre esses números em outra variável. Por último mostrando o resultado na tela

---
+++ SomaInteiros.java
@@ -0,0 +1,13 @@
+public class SomaInteiros {
+    public static void main(String[] args) {
+        // declarando e iniciando as variáveis
+        int n1 = 2;
+        int n2 = 3;
+
+        // calcula a soma entre elas
+        int soma = n1 + n2;
+
+        // mostra o resultado na tela
+        System.out.println(n1 + " + " + n2 + " = " + soma);
+    }
+}
